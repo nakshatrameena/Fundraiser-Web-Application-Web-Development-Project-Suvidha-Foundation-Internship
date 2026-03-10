9d13f6aa6d2382d11a6d8c141aebee89bc40330c
NAKSHATRA MEENA <nakshatra301407@gmail.com>
2026-03-10 19:02:48 +0530

    fix mongodb atlas connection for render

--- fundraiser-app/backend/config/db.js
+++ fundraiser-app/backend/config/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/fundraiserDB"); // No options needed
-    console.log("MongoDB connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
   } catch (err) {
     console.error("Database connection failed:", err.message);
     process.exit(1);
